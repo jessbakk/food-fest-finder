0587d0c84e645596febef3fafd9de03a7e290f40
Jess Bakkers <jessica.bakkers@gmail.com>
2017-09-05 09:56:43 -0700

    added EventsPage and route to direct to the page

---
+++ controllers/events.js
@@ -0,0 +1,7 @@
+function index(req, res) {
+  console.log("made it to the events controller");
+}
+
+module.exports = {
+  index
+}
---
+++ src/pages/EventsPage/EventsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const EventsPage = () => {
+  return (
+  <div>
+    <h1>EventsPage is here</h1>
+  </div>
+  );
+};
+
+
+export default EventsPage;
