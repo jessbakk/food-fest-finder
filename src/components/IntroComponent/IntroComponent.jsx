765feea12ed757faa41765ab42a8a291430128c3
Jess Bakkers <jessica.bakkers@gmail.com>
2017-09-06 09:03:15 -0700

    commit before deploying

--- src/components/IntroComponent/IntroComponent.jsx
+++ src/components/IntroComponent/IntroComponent.jsx
@@ -5,7 +5,7 @@ const IntroComponent = () => {
   return (
     <div className="container">
       <h1 className="row text-center">Find FoodFests around your area!</h1>
-      <div >What</div>
+      <div className="col" >What</div>
       <div>Why</div>
       <div>How</div>
     </div>
