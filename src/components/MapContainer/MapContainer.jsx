f22cc2ca7923e9f878fcc3fc6a6bd14ac21bbb05
Jess Bakkers <jessica.bakkers@gmail.com>
2017-09-07 12:49:29 -0700

    map centers to LA

--- src/components/MapContainer/MapContainer.jsx
+++ src/components/MapContainer/MapContainer.jsx
@@ -6,14 +6,51 @@ import './MapContainer.css';
 export class MapContainer extends Component {
   constructor(props) {
     super(props);
-   
+    this.state={
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedEvent: {}
+    }
+
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClicked = this.onMapClicked.bind(this);
+  }
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedEvent: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+
+  onMapClicked = (props) => {
+    if(this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
   }
 
   render() {
     return (
       <div id="map-container">
-        <Map google={this.props.google} zoom={14}>
-        <Marker onClick={this.onMarkerClick} name={'Current Location'} />
+        <Map 
+          initialCenter={{lat: 34.0522, lng: -118.2437}}
+          google={this.props.google} zoom={14}>
+        <Marker 
+          onClick={this.onMarkerClick} 
+          name={'Current Location'} 
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+          <div>
+            <h2>{this.state.selectedEvent.name}</h2>
+            <h4>{this.state.selectedEvent.description}</h4>
+          </div>
+        </InfoWindow>
         </Map>
       </div>
     );
