88d5f676f2d5811c64803fdc86bb9918771410a5
Jess Bakkers <jessica.bakkers@gmail.com>
2017-09-07 10:08:00 -0700

    adjusted addevent form

--- src/components/AddEventForm/AddEventForm.jsx
+++ src/components/AddEventForm/AddEventForm.jsx
@@ -42,36 +42,38 @@ class AddEventForm extends Component {
   }
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>Event Name</label>
-        <input value={this.state.name} onChange={(e) => this.handleChange('name', e)} />
-        <label>Description</label>
-        <input value={this.state.description} onChange={(e) => this.handleChange('description', e)} />
-        <label>Month</label>
-        <input value={this.state.month} onChange={(e) => this.handleChange('month', e)} />
-        <label>Day(s)</label>
-        <input value={this.state.day} onChange={(e) => this.handleChange('day', e)} />
-        <label>Year</label>
-        <input value={this.state.year} onChange={(e) => this.handleChange('year', e)} />
-        <label>Day(s) of the Week</label>
-        <input value={this.state.dayOfWeek} onChange={(e) => this.handleChange('dayOfWeek', e)} />
-        <label>Time</label>
-        <input value={this.state.time} onChange={(e) => this.handleChange('time', e)} />
-        <label>Location</label>
-        <input value={this.state.location} onChange={(e) => this.handleChange('location', e)} />
-        <label>City</label>
-        <input value={this.state.city} onChange={(e) => this.handleChange('city', e)} />
-        <label>Cost</label>
-        <input value={this.state.cost} onChange={(e) => this.handleChange('cost', e)} />
-        <label>Website</label>
-        <input value={this.state.website} onChange={(e) => this.handleChange('website', e)} />
-        <label>Photos</label>
-        <input value={this.state.photos} onChange={(e) => this.handleChange('photos', e)} />
-        
-        <input type="submit" value="Submit" />
+      <div className="container">
+        <form onSubmit={this.handleSubmit}>
+          <label>Event Name</label>
+          <input value={this.state.name} onChange={(e) => this.handleChange('name', e)} />
+          <label>Description</label>
+          <input value={this.state.description} onChange={(e) => this.handleChange('description', e)} />
+          <label>Month</label>
+          <input value={this.state.month} onChange={(e) => this.handleChange('month', e)} />
+          <label>Day(s)</label>
+          <input value={this.state.day} onChange={(e) => this.handleChange('day', e)} />
+          <label>Year</label>
+          <input value={this.state.year} onChange={(e) => this.handleChange('year', e)} />
+          <label>Day(s) of the Week</label>
+          <input value={this.state.dayOfWeek} onChange={(e) => this.handleChange('dayOfWeek', e)} />
+          <label>Time</label>
+          <input value={this.state.time} onChange={(e) => this.handleChange('time', e)} />
+          <label>Location</label>
+          <input value={this.state.location} onChange={(e) => this.handleChange('location', e)} />
+          <label>City</label>
+          <input value={this.state.city} onChange={(e) => this.handleChange('city', e)} />
+          <label>Cost</label>
+          <input value={this.state.cost} onChange={(e) => this.handleChange('cost', e)} />
+          <label>Website</label>
+          <input value={this.state.website} onChange={(e) => this.handleChange('website', e)} />
+          <label>Photos</label>
+          <input value={this.state.photos} onChange={(e) => this.handleChange('photos', e)} />
+          
+          <input type="submit" value="Submit" />
 
-        <Link to='/events'>Cancel</Link>
-      </form>
+          <Link to='/events'>Cancel</Link>
+        </form>
+      </div>
     );
   }
 }
