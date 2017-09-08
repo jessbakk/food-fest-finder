7e006cb5ca78412fc58c9f804324033232a127ca
Jess Bakkers <jessica.bakkers@gmail.com>
2017-09-07 17:24:52 -0700

    image centered without bootstrap and text centered for now

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -6,10 +6,10 @@ const Event = (props) => {
   return (
         <div>
           <div className="card card-styling">
-            <img className="card-img-top" src={props.event.photos[0]} className="img-thumbnail" alt="" />
+            <img className="card-img-top align-center" src={props.event.photos[0]} className="img-thumbnail" alt="" />
             <div className="card-block">
-              <h3 className="card-text">{props.event.name}</h3>
-              <p className="card-text">{props.event.description}</p>
+              <h3 className="card-text text-center">{props.event.name}</h3>
+              <p className="card-text text-center">{props.event.description}</p>
             </div>
           </div>
         </div>
