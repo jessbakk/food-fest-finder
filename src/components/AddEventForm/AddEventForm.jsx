146f738de2a587c2b1515ef47194349321f9fc44
Jess Bakkers <jessica.bakkers@gmail.com>
2017-09-08 10:38:37 -0700

    added NavBar to addevent page and run build to prepare for heroku

--- src/components/AddEventForm/AddEventForm.jsx
+++ src/components/AddEventForm/AddEventForm.jsx
@@ -42,65 +42,65 @@ class AddEventForm extends Component {
   }
   render() {
     return (
-      <div className="container">
+      <div>
         <form className="form-horizontal" onSubmit={this.handleSubmit}>
           <div className="form-group">
-            <div className="col-sm-6">
+            <div className="col-sm-12">
               <input type="text" className="form-control" placeholder="Event Name" value={this.state.name} onChange={(e) => this.handleChange('name', e)} />
             </div>
           </div>
           <div className="form-group">
-            <div className="col-sm-6">
+            <div className="col-sm-12">
               <input type="text" className="form-control" placeholder="Description" value={this.state.description} onChange={(e) => this.handleChange('description', e)} />
             </div>
           </div>
           <div className="form-group">
-            <div className="col-sm-6">
+            <div className="col-sm-12">
               <input type="text" className="form-control" placeholder="Month" value={this.state.month} onChange={(e) => this.handleChange('month', e)} />
             </div>
           </div>
           <div className="form-group">
-            <div className="col-sm-6">
+            <div className="col-sm-12">
               <input type="text" className="form-control" placeholder="Day(s)" value={this.state.day} onChange={(e) => this.handleChange('day', e)} />
             </div>
           </div>
           <div className="form-group">
-            <div className="col-sm-6">
+            <div className="col-sm-12">
               <input type="text" className="form-control" placeholder="Year" value={this.state.year} onChange={(e) => this.handleChange('year', e)} />
             </div>
           </div>
           <div className="form-group">
-            <div className="col-sm-6">
+            <div className="col-sm-12">
               <input type="text" className="form-control" placeholder="Day(s) of the Week" value={this.state.dayOfWeek} onChange={(e) => this.handleChange('dayOfWeek', e)} />
             </div>
           </div>
           <div className="form-group">
-            <div className="col-sm-6">
+            <div className="col-sm-12">
               <input type="text" className="form-control" placeholder="Time" value={this.state.time} onChange={(e) => this.handleChange('time', e)} />
             </div>
           </div>
           <div className="form-group">
-            <div className="col-sm-6">
+            <div className="col-sm-12">
               <input type="text" className="form-control" placeholder="Location" value={this.state.location} onChange={(e) => this.handleChange('location', e)} />
             </div>
           </div>
           <div className="form-group">
-            <div className="col-sm-6">
+            <div className="col-sm-12">
               <input type="text" className="form-control" placeholder="City" value={this.state.city} onChange={(e) => this.handleChange('city', e)} />
             </div>
           </div>
           <div className="form-group">
-            <div className="col-sm-6">
+            <div className="col-sm-12">
               <input type="text" className="form-control" placeholder="Cost($)" value={this.state.cost} onChange={(e) => this.handleChange('cost', e)} />
             </div>
           </div>
           <div className="form-group">
-            <div className="col-sm-6">
+            <div className="col-sm-12">
               <input type="text" className="form-control" placeholder="Website" value={this.state.website} onChange={(e) => this.handleChange('website', e)} />
             </div>
           </div>
           <div className="form-group">
-            <div className="col-sm-6">
+            <div className="col-sm-12">
               <input type="text" className="form-control" placeholder="Photos (url)" value={this.state.photos} onChange={(e) => this.handleChange('photos', e)} />
             </div>
           </div>
--- src/pages/AddEventPage/AddEventPage.jsx
+++ src/pages/AddEventPage/AddEventPage.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import AddEventForm from '../../components/AddEventForm/AddEventForm';
+import NavBar from '../../components/NavBar/NavBar';
 
 
 const AddEventPage = (prop) => {
   return (
     <div className="container">
-      <h1>Add Event</h1>
-      <AddEventForm />
+      <div className="row">
+        <NavBar />
+        <br />
+        <h1>Add Event</h1>
+        <AddEventForm />
+      </div>
     </div>
   )
 }
