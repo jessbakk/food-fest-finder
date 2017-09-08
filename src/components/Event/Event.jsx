e2f2d2575d2d18d60dedd114ca9b956f99626095
Jess Bakkers <jessica.bakkers@gmail.com>
2017-09-08 10:20:29 -0700

    added line break on event cards and run build to prepare for heroku

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -9,6 +9,7 @@ const Event = (props) => {
             <img className="card-img-top align-center" src={props.event.photos[0]} className="img-thumbnail" alt="" />
             <div className="card-block">
               <h3 className="card-text text-center event-title">{props.event.name}</h3>
+              <br />
               <p className="text-center event-details">{props.event.dayOfWeek[0]}, {props.event.month} {props.event.day[0]}, {props.event.year} </p>
               <br/>
               <p className="card-text text-center event-details">{props.event.description}</p>
